644423e9a7a6fd4ed463822784d3d0e999ba086f
Rem Lampa <rem@lansangan.com>
2017-06-18 17:13:35 +0000

    Implement dictionary in speller pset

--- pset5/speller/dictionary.c
+++ pset5/speller/dictionary.c
@@ -2,17 +2,56 @@
  * Implements a dictionary's functionality.
  */
 
+#include <stdlib.h>
+#include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
 
 #include "dictionary.h"
+
+// trie struct declaration
+struct _node
+{
+    bool is_word;
+
+    struct _node * nodes[27];
+};
+
+// dictionary pointer
+node * loaded_dictionary;
+
+// number of words loaded in dictionary
+unsigned int words;
 
 /**
  * Returns true if word is in dictionary else false.
  */
 bool check(const char *word)
 {
-    // TODO
-    return false;
+    node * current_node = loaded_dictionary;
+    
+    const unsigned int length = strlen(word);
+    
+    for (int i = 0; i <= length; i++)
+    {
+        // end of word
+        if (i == length)
+        {
+            if  (current_node->is_word == false) return false;
+        }
+        else {
+            unsigned int index = shift_index(word[i]);
+            
+            // no word has the current letter in its sequence        
+            if (current_node->nodes[index] == NULL) return false;
+            
+            // move on to next letter
+            current_node = current_node->nodes[index];
+        }
+    }
+    
+    return true;
 }
 
 /**
@@ -20,8 +59,51 @@ bool check(const char *word)
  */
 bool load(const char *dictionary)
 {
-    // TODO
-    return false;
+    // open dictionary file
+    FILE *fp = fopen(dictionary, "r");
+    
+    // file open failed
+    if (fp == NULL) return false;
+    
+    // initialize dictionary trie node
+    loaded_dictionary = init_node();
+    
+    words = 0;
+    
+    node * current_node = loaded_dictionary;
+    
+    // read file one character at a time
+    for (int c = fgetc(fp); c != EOF; c = fgetc(fp))
+    {
+        // end of line, thus end of word
+        if (c == '\n')
+        {
+            current_node->is_word = true;
+            
+            current_node = loaded_dictionary;
+            
+            words++;
+        }
+        else
+        {
+            unsigned int index = shift_index(c);
+            
+            // if node for current letter does not exist yet, initialize a node for it
+            if (current_node->nodes[index] == NULL) current_node->nodes[index] = init_node();
+
+            // move to the next node
+            current_node = current_node->nodes[index];
+        }
+    }
+    
+    // check if file read errors occured
+    if (ferror(fp)) return false;
+    
+    // close file
+    fclose(fp);
+
+    // successfully loaded dictionary
+    return true;
 }
 
 /**
@@ -29,8 +111,7 @@ bool load(const char *dictionary)
  */
 unsigned int size(void)
 {
-    // TODO
-    return 0;
+    return words;
 }
 
 /**
@@ -38,6 +119,46 @@ unsigned int size(void)
  */
 bool unload(void)
 {
-    // TODO
-    return false;
+    free_node(loaded_dictionary);
+    
+    return true;
+}
+
+/**
+ * Initialize a node
+ */
+node * init_node(void)
+{
+    node * new_node = malloc(sizeof(node));
+    
+    new_node->is_word = false;
+    
+    // set all sub-nodes to NULL
+    for(int i = 0; i < 27; i++) new_node->nodes[i] = NULL;
+    
+    return new_node;
+}
+
+/**
+ * Deallocate memory given to a node
+ */
+void free_node(node * current_node)
+{
+    for (int i = 0; i < 27; i++)
+    {
+        if (current_node->nodes[i] != NULL)
+        {
+            free_node(current_node->nodes[i]);
+        }
+    }
+
+    free(current_node);
+}
+
+/**
+ * Shifts ASCII to zero index
+ */
+unsigned int shift_index(char c)
+{
+    return c == '\'' ? 26 : tolower(c) - 97;
 }
--- pset5/speller/dictionary.h
+++ pset5/speller/dictionary.h
@@ -5,11 +5,18 @@
 #ifndef DICTIONARY_H
 #define DICTIONARY_H
 
+#include <stdlib.h>
+#include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
 
 // maximum length for a word
 // (e.g., pneumonoultramicroscopicsilicovolcanoconiosis)
 #define LENGTH 45
+
+// dictionary trie node
+typedef struct _node node;
 
 /**
  * Returns true if word is in dictionary else false.
@@ -30,5 +37,20 @@ unsigned int size(void);
  * Unloads dictionary from memory.  Returns true if successful else false.
  */
 bool unload(void);
+
+/**
+ * Initialize a node
+ */
+node * init_node(void);
+
+/**
+ * Deallocate memory given to a node
+ */
+void free_node(node * current_node);
+
+/**
+ * Shifts ASCII to zero index
+ */
+unsigned int shift_index(char c);
 
 #endif // DICTIONARY_H
