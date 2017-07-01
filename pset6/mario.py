788690e114795260dc36140824ea6deca48693b4
Rem Lampa <rem@lansangan.com>
2017-07-01 06:27:11 +0000

    Implement proper usage of Python range in Mario pset

--- pset6/mario.py
+++ pset6/mario.py
@@ -11,20 +11,20 @@ while True:
         print('Height must be between 0 and 23')
         print()
 
-for i in range(0, n):
+for i in range(n):
     # Add left side spaces
-    for j in range(0, n-(i+1)):
+    for j in range(n-(i+1)):
         print(' ', end='')
     
     # Add left side hashes
-    for j in range(0, i+1):
+    for j in range(i+1):
         print('#', end='')
     
     # Add middle spaces
     print('  ', end='')
     
     # Add right side hashes
-    for j in range(0, i+1):
+    for j in range(i+1):
         print('#', end='')
         
     print()
